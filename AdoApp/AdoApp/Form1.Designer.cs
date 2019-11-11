e6b305ef6762975ff2434a3f7f4729a4cbe99668
Mejis <onuragici@slnyazilim.com>
2019-11-11 11:05:40 +0300

    NewFiles
    new

---
+++ AdoApp/AdoApp/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace AdoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUnvan = new System.Windows.Forms.ComboBox();
+            this.txtDoktor = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.Liste = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unvan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Doktor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUnvan
+            // 
+            this.txtUnvan.FormattingEnabled = true;
+            this.txtUnvan.Location = new System.Drawing.Point(134, 39);
+            this.txtUnvan.Name = "txtUnvan";
+            this.txtUnvan.Size = new System.Drawing.Size(121, 21);
+            this.txtUnvan.TabIndex = 0;
+            // 
+            // txtDoktor
+            // 
+            this.txtDoktor.Location = new System.Drawing.Point(134, 75);
+            this.txtDoktor.Name = "txtDoktor";
+            this.txtDoktor.Size = new System.Drawing.Size(121, 20);
+            this.txtDoktor.TabIndex = 1;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(134, 111);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(121, 20);
+            this.txtTel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ünvan :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Doktor Adı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon:";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(398, 45);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(398, 78);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(398, 109);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // Liste
+            // 
+            this.Liste.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Liste.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Unvan,
+            this.Doktor,
+            this.Tel});
+            this.Liste.Location = new System.Drawing.Point(12, 161);
+            this.Liste.Name = "Liste";
+            this.Liste.Size = new System.Drawing.Size(479, 150);
+            this.Liste.TabIndex = 4;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Unvan
+            // 
+            this.Unvan.HeaderText = "Unvan";
+            this.Unvan.Name = "Unvan";
+            // 
+            // Doktor
+            // 
+            this.Doktor.HeaderText = "Doktor";
+            this.Doktor.Name = "Doktor";
+            // 
+            // Tel
+            // 
+            this.Tel.HeaderText = "Telefon";
+            this.Tel.Name = "Tel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 323);
+            this.Controls.Add(this.Liste);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtDoktor);
+            this.Controls.Add(this.txtUnvan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtUnvan;
+        private System.Windows.Forms.TextBox txtDoktor;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView Liste;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unvan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Doktor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tel;
+    }
+}
+
---
+++ AdoApp/AdoApp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoApp
+{
+    //ADO.Net (ActiveXDataObject.Net)Aktivex tabanlı çalışan microsoft veri erişim teknolojisidir.
+    //Bu veri erişim sadece mssql değil diğer databaseler içinde uygulanabilir.
+    //Connection:Çalıştırılacak olan sorgununhangi bağlantı üzerinde işlem yapacağı belirtilmektedir.
+    //CommandType: Çalıştırılacak olan sql cümlesinin tipini belirlemek için kullanılır.
+    //CommandText:Çalıştırılacak olan sorgu cümlesi yazılmaktadır.
+    //Transection:Çalıştırılacak olan sorguların kontrollü bir şekilde işlenmesini sağlamaktadır. Genellikle Insert,Update ve Delete işlemleri için kullanılmaktadır.
+    //Parameters: T-Sql cümlesi yada Stored Procedure içerisinde kullanılan parametreleri setlemek için kullanılır.
+
+    //Execute Metodları
+    //-->ExecuteReader:Çalıştırılan sql sorgusundan geri birden fazla değer dönecekse bu metod kullanılır.
+    //-->ExecuteScalar:Çalıştırılan sorgudan tek değer dönecekse bu komut kullanılır.
+    //-->ExecuteNonQuery:sorgudan geriye veri dönmediğinde bu metod kullanılır. Update/Delete/Insert işlemlerin kullanılır.
+    public partial class Form1 : Form
+    {
+        SqlCommand uygula;
+        SqlDataReader oku;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["CON"].ToString());
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Combo();
+            Listele();
+        }
+
+        private void Listele()
+        {
+            Liste.Rows.Clear();
+            int i = 0;
+            conn.Open();
+            uygula = new SqlCommand("select * from tblDoktors", conn);
+            oku = uygula.ExecuteReader();
+            foreach(var k in oku)
+            {
+                Liste.Rows.Add();
+                Liste.Rows[i].Cells[0].Value =oku[0];
+                Liste.Rows[i].Cells[1].Value =oku[1];
+                Liste.Rows[i].Cells[2].Value =oku[2];
+                Liste.Rows[i].Cells[3].Value =oku[3];
+
+                i++;
+            }
+            Liste.AllowUserToAddRows = false;
+            oku.Close();
+            conn.Close();
+        }
+
+        private void Combo()
+        {
+            string query;
+            query = "Select * from tblUnvan";
+            uygula = new SqlCommand(query, conn);
+            uygula.Connection.Open();
+            oku = uygula.ExecuteReader(CommandBehavior.CloseConnection);
+            foreach(var k in oku)
+            {
+                txtUnvan.Items.Add(oku.GetString(1));
+            }
+            conn.Close();
+            oku.Close();
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            uygula = new SqlCommand();
+            uygula.CommandType = CommandType.StoredProcedure;
+            uygula.CommandText = "[S_Doktors]";
+            uygula.Connection = conn;
+            uygula.Connection.Open();
+
+            uygula.Parameters.Add("@unvan", SqlDbType.NVarChar, 50).Value = txtUnvan.Text;
+            uygula.Parameters.Add("@doktoradi", SqlDbType.NVarChar, 50).Value = txtDoktor.Text;
+            uygula.Parameters.Add("@tel", SqlDbType.NVarChar, 50).Value = txtTel.Text;
+
+            uygula.ExecuteNonQuery();
+            uygula.Connection.Close();
+            conn.Close();
+            MessageBox.Show("Kayıt işlemi yapıldı");
+        }
+    }
+}
